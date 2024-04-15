c7135d847652d9e927afaa0efb47068272490a05
maver-ya <maver2005@ya.ru>
2024-04-15 18:29:20 +0300

    redact review logic

--- movies_app/forms.py
+++ movies_app/forms.py
@@ -1,8 +1,13 @@
 from django import forms
+
 from .models import *
 
 
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
-        fields = ['comments']
+        fields = ['movie', 'comments', 'user']
+        widgets = {
+            "movie": forms.HiddenInput(),
+            "user": forms.HiddenInput()
+        }
